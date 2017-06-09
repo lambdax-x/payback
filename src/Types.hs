8d2fc02d0f8942dc0af8826f192cd5ce3a4959d6
λx.x <lambdax-x@users.noreply.github.com>
2017-06-09 22:11:18 +0200

    Add types

---
+++ src/Types.hs
@@ -0,0 +1,23 @@
+module Types where
+
+import Data.Text
+import Data.DateTime
+
+data Transaction = Transaction { source :: User
+                               , metaData :: MetaData
+                               , reason :: Text
+                               , debtors :: [(User, Amount)]
+                               , grantees :: [(User, Amount)]
+                               }
+
+data MetaData = MetaData { dataTime :: Maybe DateTime
+                         , location :: Maybe Text
+                         }
+
+data User = User { name :: Text }
+
+data Amount = Amount { value :: Int
+                     , currency :: Currency
+                     }
+
+data Currency = EUR | CHF
