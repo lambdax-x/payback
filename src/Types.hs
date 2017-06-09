bea79bd5a453dc15ea31492eafa59c015054318d
λx.x <lambdax-x@users.noreply.github.com>
2017-06-09 23:00:16 +0200

    Remove datetime dependency, add our own DateTime type and fix records style

--- src/Types.hs
+++ src/Types.hs
@@ -1,23 +1,41 @@
 module Types where
 
 import Data.Text
-import Data.DateTime
 
-data Transaction = Transaction { source :: User
-                               , metaData :: MetaData
-                               , reason :: Text
-                               , debtors :: [(User, Amount)]
-                               , grantees :: [(User, Amount)]
-                               }
+data Transaction = Transaction
+    { source :: User
+    , metaData :: MetaData
+    , reason :: Text
+    , debtors :: [(User, Amount)]
+    , grantees :: [(User, Amount)]
+    }
 
-data MetaData = MetaData { dateTime :: Maybe DateTime
-                         , location :: Maybe Text
-                         }
+data MetaData = MetaData
+    { dataTime :: Maybe DateTime
+    , location :: Maybe Text
+    }
 
 data User = User { name :: Text }
 
-data Amount = Amount { value :: Int
-                     , currency :: Currency
-                     }
+data Amount = Amount
+    { value :: Int
+    , currency :: Currency
+    }
 
 data Currency = EUR | CHF
+
+data DateTime = DateTime
+    { date :: Date
+    , time :: Time
+    }
+
+data Date = Date
+    { day :: Int
+    , month :: Int
+    , year :: Int
+    }
+
+data Time = Time
+    { hour :: Int
+    , minutes :: Int
+    }
