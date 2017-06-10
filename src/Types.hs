1b2d18aeab05d1d738a2f46c22251080fc724e09
λx.x <lambdax-x@users.noreply.github.com>
2017-06-10 22:58:20 +0200

    Derive types with ToJSON and FromJSON instances

--- src/Types.hs
+++ src/Types.hs
@@ -1,41 +1,82 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Types where
 
+import GHC.Generics
 import Data.Text
+import Data.Aeson
+import Data.ByteString.Lazy
+
+mkTx :: ByteString -> Maybe Transaction
+mkTx = decode
 
 data Transaction = Transaction
     { source :: User
-    , metaData :: MetaData
+    , metadata :: Metadata
     , reason :: Text
     , debtors :: [(User, Amount)]
     , grantees :: [(User, Amount)]
-    }
+    } deriving (Show, Generic)
 
-data MetaData = MetaData
-    { dataTime :: Maybe DateTime
+instance FromJSON Transaction
+instance ToJSON Transaction
+
+data Metadata = Metadata
+    { datetime :: DateTime
     , location :: Maybe Text
-    }
+    } deriving (Show, Generic)
 
-newtype User = User { name :: Text } deriving (Eq, Show)
+instance FromJSON Metadata
+instance ToJSON Metadata
+
+newtype User = User { name :: Text }
+    deriving (Eq, Show, Generic)
+
+instance FromJSON User
+instance ToJSON User
 
 data Amount = Amount
     { value :: Float
     , currency :: Currency
-    } deriving (Eq, Show)
+    } deriving (Eq, Show, Generic)
 
-data Currency = EUR | CHF deriving (Eq, Show)
+instance FromJSON Amount
+instance ToJSON Amount
+
+data Currency = EUR | CHF
+    deriving (Eq, Show, Generic)
+
+instance FromJSON Currency where
+    parseJSON (String "EUR") = return EUR
+    parseJSON (String "€") = return EUR
+    parseJSON (String "CHF") = return CHF
+    parseJSON _ = error "invalid currency"
+
+instance ToJSON Currency where
+    toJSON EUR = String "€"
+    toJSON CHF = String "CHF"
 
 data DateTime = DateTime
     { date :: Date
     , time :: Time
-    }
+    } deriving (Show, Generic)
+
+instance FromJSON DateTime
+instance ToJSON DateTime
 
 data Date = Date
     { day :: Int
     , month :: Int
     , year :: Int
-    }
+    } deriving (Show, Generic)
+
+instance FromJSON Date
+instance ToJSON Date
 
 data Time = Time
     { hour :: Int
     , minutes :: Int
-    }
+    } deriving (Show, Generic)
+
+instance FromJSON Time
+instance ToJSON Time
